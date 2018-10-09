9d451035e37b473276bf0a331d4822813686378a
Rose Brennan <brennan.m.rose@gmail.com>
2018-10-09 01:35:00 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,12 +8,17 @@ class Anagram
   end 
   
   def match(array)
-    array.each do |x|
-      if x.split("").sort == x 
-        x 
-      else 
-        array = []
+ 	  value = []
+    array.each do |x| 
+      value = x if x.split("").sort.join == word.split("").sort.join
     end
-  end 
+  	%w(value)
+end
+  
+  # def match(array)
+  #   value = nil
+  #   value = x if array.each { |x| x.split("").sort.join } == word.each { |w| w.split("").sort.join }
+  #   binding.pry
+  # end
   
 end 
